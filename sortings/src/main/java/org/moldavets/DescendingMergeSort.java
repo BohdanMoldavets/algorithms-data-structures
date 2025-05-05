c00e7c6c67ae236e7f69916b5fa27cfae699f368
Bohdan <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-05 08:49:43 +0200

    Add DescendingMergeSort

--- sortings/src/main/java/org/moldavets/DescendingMergeSort.java
+++ sortings/src/main/java/org/moldavets/DescendingMergeSort.java
@@ -2,15 +2,9 @@ package org.moldavets;
 
 import java.util.Arrays;
 
-public class MergeSort {
+public class DescendingMergeSort {
     public static void main(String[] args) {
-//        int[] array = {20,35,-15,7,55,1,-22};
-//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-//        int[] array = {5, 3, 5, 3, 5, 3, 5, 3};
-//        int[] array = {9999999, -9999999, 123456, -123456, 0, 42, -42};
         int[] array = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-
         Arrays.stream(mergeSort(array)).forEach(System.out::println);
     }
 
@@ -19,7 +13,7 @@ public class MergeSort {
         int i = 0, j = 0, k = 0;
 
         while(i < firstArray.length && j < secondArray.length){
-            resultArray[k++] = firstArray[i] < secondArray[j] ? firstArray[i++] : secondArray[j++];
+            resultArray[k++] = firstArray[i] > secondArray[j] ? firstArray[i++] : secondArray[j++];
         }
 
         while (i < firstArray.length) {
